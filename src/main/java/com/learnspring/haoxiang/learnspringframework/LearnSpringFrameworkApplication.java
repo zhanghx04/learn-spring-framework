97348dceddc38253ce0546b0564b8027aace5c7f
zhanghx04 <zhanghx04@gmail.com>
2023-03-08 17:37:15 -0600

    Added spring boot to the application

--- src/main/java/com/learnspring/haoxiang/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/learnspring/haoxiang/learnspringframework/LearnSpringFrameworkApplication.java
@@ -1,13 +1,27 @@
 package com.learnspring.haoxiang.learnspringframework;
 
+import com.learnspring.haoxiang.learnspringframework.enterprise.example.web.MyWebController;
+import com.learnspring.haoxiang.learnspringframework.game.GameRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+//		MarioGame game = new MarioGame();
+//		SuperContraGame game = new SuperContraGame();
+//		PacmanGame game = new PacmanGame();
+//		GameRunner runner = new GameRunner(game);
+//		runner.run();
+
+		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		GameRunner runner = context.getBean(GameRunner.class);
+		runner.run();
+
+		MyWebController controller = context.getBean(MyWebController.class);
+		System.out.println(controller.returnValueFromBusinessService());
 	}
 
 }
---
+++ src/main/java/com/learnspring/haoxiang/learnspringframework/game/GamingConsole.java
@@ -0,0 +1,8 @@
+package com.learnspring.haoxiang.learnspringframework.game;
+
+public interface GamingConsole {
+    void up();
+    void down();
+    void left();
+    void right();
+}
